35534807b389eeb2dec51cc5b13331e6f05061c5
Kresten Laust <krestenlaust@gmail.com>
2023-12-03 00:26:08 +0100

    Finished day 1 assignment 1

--- Day1/Assignment1.fs
+++ Day1/Assignment1.fs
@@ -1,3 +1,31 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
 
+let stringToCharArray (str : string) : char list =
+    str |> Seq.toList
+
+let charArrayToString (str : char list) : string =
+    str |> Array.ofList |> String
+
+let rec exclusivelyNumbers =
+    List.filter Char.IsDigit
+
+let firstAndLastElement str =
+    [Seq.head str; Seq.last str]
+
+let findFirstAndLastNumber str =
+    str
+    |> stringToCharArray
+    |> exclusivelyNumbers
+    |> firstAndLastElement
+    |> charArrayToString
+
+let calibrationValue =
+    findFirstAndLastNumber >> int
+
+let sumOfAllLines =
+    Seq.sumBy calibrationValue
+
+File.ReadLines "input.txt"
+|> sumOfAllLines
+|> printfn "%i"
