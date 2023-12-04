fad42a4513fc9946c3e32412253ca4341f132c31
Kresten Laust <krestenlaust@gmail.com>
2023-12-04 14:56:49 +0100

    Work in progress

---
+++ Day3/Assignment1.fs
@@ -0,0 +1,13 @@
+﻿open System
+open System.IO
+
+let sumOfPartNumbers (engineSchematic : char list seq) =
+    
+
+let sumOfPartNumbersString (engineSchematic : string) =
+    Seq.map Seq.toList (engineSchematic.Split '\n')
+
+
+File.ReadAllText "input.txt"
+|> sumOfPartNumbersString
+|> sumOfPartNumbers
